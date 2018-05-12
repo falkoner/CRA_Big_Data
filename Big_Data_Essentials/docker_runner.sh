ba34b75081eca2fc2ce8ce1e4d650988ce75a211
Alex Filatau <filatau@gmail.com>
2018-05-12 07:29:48 -0700

    Add solution for spark shortest path calc and TF-IDF

--- Big_Data_Essentials/docker_runner.sh
+++ Big_Data_Essentials/docker_runner.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# docker run --rm -it -d -p 8888:8888 -p 50070:50070 -p 8088:8088 --name big_hadoop bigdatateam/hdfs-notebook
-docker run --rm -it -d -p 8888:8888 -p 50070:50070 -p 8088:8088 -p 4040:4040 --name big_spark bigdatateam/spark-course1
+ docker run --rm -it -d -p 8888:8888 -p 50070:50070 -p 8088:8088 --name big_hadoop bigdatateam/hdfs-notebook
+# docker run --rm -it -d -p 8888:8888 -p 50070:50070 -p 8088:8088 -p 4040:4040 --name big_spark bigdatateam/spark-course1
+#docker run --rm -it -d -p 8888:8888 -p 50070:50070 -p 8088:8088 -p 4040:4040 --name big_yarn bigdatateam/yarn-notebook 
